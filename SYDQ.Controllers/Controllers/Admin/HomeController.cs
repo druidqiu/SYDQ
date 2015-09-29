860f2472083028d4aab91bbb20ed48050221ede2
druidqiu <guoxin_qiu@126.com>
2015-09-29 08:31:47 +0800

    add test controller

---
+++ SYDQ.Controllers/Controllers/Admin/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SYDQ.Controllers.Controllers.Admin
+{
+    public class HomeController : AdminBaseController
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SYDQ.Controllers/Controllers/Admin/UserController.cs
@@ -0,0 +1,36 @@
+﻿using SYDQ.IServices.Interfaces;
+using SYDQ.IServices.Messaging.UserService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SYDQ.Controllers.Controllers.Admin
+{
+    public class UserController : AdminBaseController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: Client/User
+        public ActionResult Index(int page = 1, string username = "", string email = "")
+        {
+            GetPagedUserViewRequest request = new GetPagedUserViewRequest()
+            {
+                PageIndex = page,
+                PageSize = 1,
+                Username = username,
+                EmailAddress = email
+            };
+            var response = _userService.GetPagedUserView(request);
+            
+            return View(response);
+        }
+    }
+}
